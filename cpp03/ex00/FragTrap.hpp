79957c8b951974d3852373fc5cb36c72febd363e
ChoiKanghun <rkdgns4562@naver.com>
2021-01-21 20:39:33 +0900

    cpp03-01done

---
+++ cpp03/ex00/FragTrap.hpp
@@ -0,0 +1,43 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include <iostream>
+#include <time.h>
+#include <string>
+
+class FragTrap
+{
+private:
+	unsigned int	_hit_points;
+	unsigned int	_max_hit_points;
+	unsigned int	_energy_points;
+	unsigned int	_max_energy_points;
+	unsigned int	_level;
+	std::string	_name;
+	unsigned int	_melee_attack_damage;
+	unsigned int	_ranged_attack_damage;
+	unsigned int	_armor_damage_reduction;
+
+	void		cub3dAttack(std::string const &target);
+	void		miniRTAttack(std::string const &target);
+	void		minishellAttack(std::string const &target);
+	void		printfAttack(std::string const &target);
+	void		libasmAttack(std::string const &target);
+
+public:
+	FragTrap();
+	FragTrap(std::string name);
+	FragTrap(const FragTrap&);
+	virtual ~FragTrap();
+
+	void		rangedAttack(std::string const &target);
+	void		meleeAttack(std::string const &target);
+	void		takeDamage(unsigned int amount);
+	void		beRepaired(unsigned int amount);
+	void		vaulthunter_dot_exe(std::string const &target);
+		
+};
+
+
+
+#endif
