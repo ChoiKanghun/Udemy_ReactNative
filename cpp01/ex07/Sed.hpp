ac541afa2ce47e562ed6ee7a7f6f07bfd6488f6c
ChoiKanghun <rkdgns4562@naver.com>
2021-01-31 22:40:17 +0900

    before get evaluation cpp 01

--- cpp01/ex07/Sed.hpp
+++ cpp01/ex07/Sed.hpp
@@ -7,12 +7,10 @@
 
 class Sed
 {
-private:
-	static std::string strReplace(std::string const &line, std::string const &search, std::string const &replace);
 public:
 	Sed();
 	virtual ~Sed();
-
+	static std::string strReplace(std::string const &line, std::string const &search, std::string const &replace);
 	static void replace(std::string const &filename, std::string const &search, std::string const &replace);
 };
 
